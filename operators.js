188493fc52017114f61cdbfa70092185829bd210
Ryan Helsing <ryanhelsing@gmail.com>
2016-11-20 15:45:34 -0600

    cool

--- operators.js
+++ operators.js
@@ -1,6 +1,14 @@
 //node --use_strict operators.js
 
-var age = 26
+var age = 13
 var status = (age >= 18) ? "adult" : "minor" //ternary
 console.log(status)
 
+//dont need ugly curlies :)
+for (var i = 0, j = 9; i <= j; i++, j--)
+  console.log("a[" + i + "][" + j + "]")
+
+if (status == "adult")
+  console.log("hey")
+else
+  console.log("bye")
